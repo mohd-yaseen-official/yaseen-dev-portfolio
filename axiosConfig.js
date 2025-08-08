2b71421065b9d7acccf7305ee8c27b760ae8215a
Mohamed Yaseen <yaseenichu21@gmail.com>
2025-08-08 22:26:15 +0530

    Added URLs to Axios Config for production

--- axiosConfig.js
+++ axiosConfig.js
@@ -2,44 +2,44 @@ import axios from "axios";
 
 
 export const featuredSkillsConfig = axios.create({
-    baseURL: "https://mohamed-yaseen.info/api/v1/skills/?is_featured=true",
+    baseURL: "https://mohamedyaseen.developer.li/api/v1/skills/?is_featured=true",
 });
 
 export const timelineConfig = axios.create({
-    baseURL: "https://mohamed-yaseen.info/api/v1/timeline",
+    baseURL: "https://mohamedyaseen.developer.li/api/v1/timeline",
 });
 
 export const skillTypesConfig = axios.create({
-    baseURL: "https://mohamed-yaseen.info/api/v1/skills/types",
+    baseURL: "https://mohamedyaseen.developer.li/api/v1/skills/types",
 });
 
 export const skillsConfig = axios.create({
-    baseURL: "https://mohamed-yaseen.info/api/v1/skills/?is_listed=true",
+    baseURL: "https://mohamedyaseen.developer.li/api/v1/skills/?is_listed=true",
 });
 
 export const featuredProjectsConfig = axios.create({
     baseURL:
-        "https://mohamed-yaseen.info/api/v1/portfolio/?category=project&is_featured=true",
+        "https://mohamedyaseen.developer.li/api/v1/portfolio/?category=project&is_featured=true",
 });
 
 export const featuredClonesConfig = axios.create({
     baseURL:
-        "https://mohamed-yaseen.info/api/v1/portfolio/?category=clone&is_featured=true",
+        "https://mohamedyaseen.developer.li/api/v1/portfolio/?category=clone&is_featured=true",
 });
 
 export const projectsConfig = axios.create({
-    baseURL: "https://mohamed-yaseen.info/api/v1/portfolio/?category=project",
+    baseURL: "https://mohamedyaseen.developer.li/api/v1/portfolio/?category=project",
 });
 
 export const clonesConfig = axios.create({
-    baseURL: "https://mohamed-yaseen.info/api/v1/portfolio/?category=clone",
+    baseURL: "https://mohamedyaseen.developer.li/api/v1/portfolio/?category=clone",
 });
 
 export const contactConfig = axios.create({
-    baseURL: "https://mohamed-yaseen.info/api/v1/contact",
+    baseURL: "https://mohamedyaseen.developer.li/api/v1/contact",
 });
 
 export const socialLinksConfig = axios.create({
-    baseURL: "https://mohamed-yaseen.info/api/v1/contact/social",
+    baseURL: "https://mohamedyaseen.developer.li/api/v1/contact/social",
 });
 
